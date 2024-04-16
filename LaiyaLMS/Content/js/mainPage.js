cd3fb4421056d0cda7da63130c1eeba24f82920d
Nuwan Dilina Hettiarachchi <nuwan.dilina@hayleysadvantis.com>
2024-04-16 15:34:55 +0530

    template changes

---
+++ LaiyaLMS/Content/js/mainPage.js
@@ -0,0 +1,13 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main_container_id").style.marginLeft = "250px";
+    document.getElementById("main_container_id").style.width = "83%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main_container_id").style.marginLeft = "0";
+    document.getElementById("main_container_id").style.width = "100%";
+}
